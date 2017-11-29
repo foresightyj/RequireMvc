4470dfccc80a1e0ba23d7d870ad71fe851ce5b7c
Yuan Jian <imyuanjian@gmail.com>
2017-11-30 00:18:27 +0800

    initial commit

---
+++ HelloMvcBundle/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HelloMvcBundle.MvcApplication" Language="C#" %>
---
+++ HelloMvcBundle/MvcBundle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Optimization;
+
+namespace HelloMvcBundle
+{
+    public static class MvcBundle
+    {
+        public static IHtmlString RenderScript(string path)
+        {
+            var bundleId = "script-bundle" + new Regex(@"\W+").Replace(path, "-").ToLower();
+            var count = HttpContext.Current.Items[bundleId] as int?;
+            count = (count ?? 0) + 1;
+            HttpContext.Current.Items[bundleId] = count;
+
+            if(count == 1)
+            {
+                return Scripts.RenderFormat("<script type='text/javascript' class='" + bundleId + "'>{0}</script>", path);
+            }
+            return new HtmlString(string.Format("<!-- {0} -->", bundleId + " is rendered the "+count+"-th time"));
+        }
+    }
+}
